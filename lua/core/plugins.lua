0c2fb22ada69bb91153b6d59604382186d36cded
Slaven Begović <slavenbeg@gmail.com>
2024-05-15 07:47:41 +0200

    Add Lazy border. Separate Lazy plugins and opts.

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,51 +1,35 @@
-require("lazy").setup({
+local plugins ={
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   "tpope/vim-commentary",
   "nvim-tree/nvim-tree.lua",
   "nvim-tree/nvim-web-devicons",
-  --"ellisonleao/gruvbox.nvim",
-  --"dracula/vim",
   "nvim-lualine/lualine.nvim",
   "nvim-treesitter/nvim-treesitter",
-  --"vim-test/vim-test",
-  --"lewis6991/gitsigns.nvim",
-  --"preservim/vimux",
-  --"christoomey/vim-tmux-navigator",
-  --"tpope/vim-fugitive",
   "tpope/vim-surround",
   "stevearc/oil.nvim",
-  -- completion
-  --"hrsh7th/nvim-cmp",
-  --"hrsh7th/cmp-nvim-lsp",
-  --"L3MON4D3/LuaSnip",
-  --"saadparwaiz1/cmp_luasnip",
-  --"rafamadriz/friendly-snippets",
-  --"github/copilot.vim",
-  --"williamboman/mason.nvim",
-  --"williamboman/mason-lspconfig.nvim",
-  --"neovim/nvim-lspconfig",
- -- {
- --   "vinnymeller/swagger-preview.nvim",
- --   run = "npm install -g swagger-ui-watcher",
- -- },
- -- {
- --   "iamcco/markdown-preview.nvim",
- --   run = "cd app && npm install",
- -- },
   {
-    "nvim-telescope/telescope.nvim", tag = "0.1.6",
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
     dependencies = { "nvim-lua/plenary.nvim" }
   },
   -- Visualize buffers as tabs
-  { 'akinsho/bufferline.nvim', 
-    version = '*',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  { "akinsho/bufferline.nvim", 
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
   },
   {
-    'windwp/nvim-autopairs',
+    "windwp/nvim-autopairs",
     event = "InsertEnter",
     config = true
     -- use opts = {} for passing setup options
     -- this is equalent to setup({}) function
   },
-})
+}
+
+opts = {
+  ui = {
+    border = "rounded",
+  },
+}
+
+require("lazy").setup(plugins, opts)
