b6c9d5b8a8f46bda2d5ee40bbb2b30886ee5b704
Slaven Begović <slavenbeg@gmail.com>
2024-05-17 07:28:08 +0200

    Add neo-tree

---
+++ lua/core/plugin_config/neotree.lua
@@ -0,0 +1,10 @@
+require("neo-tree").setup({
+  popup_border_style = "rounded",
+  window = {
+    position = "left",
+    width = 35,
+  },
+})
+
+vim.keymap.set("n", "<leader>e", ":Neotree filesystem toggle<CR>", {})
+vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
