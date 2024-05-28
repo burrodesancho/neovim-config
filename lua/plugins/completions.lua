57be202747a1144d4a7e9e9222f765ba326d4199
Slaven Begović <slavenbeg@gmail.com>
2024-05-28 20:13:17 +0200

    Add mapping for jumping between autocomplete placeholders

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -6,11 +6,24 @@ return {
       "saadparwaiz1/cmp_luasnip",
       "rafamadriz/friendly-snippets",
     },
+    configure = function()
+      local luasnip = require("luasnip")
+      vim.keymap.set({"i", "s"}, "<Tab>",
+        function()
+          if luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+          end
+        end,
+        {silent = true})
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
@@ -29,6 +42,18 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          -- <c-l> will move you to the right of each of the expansion locations.
+          -- <c-h> is similar, except moving you backwards.
+          ['<C-l>'] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { 'i', 's' }),
+          ['<C-h>'] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { 'i', 's' }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
