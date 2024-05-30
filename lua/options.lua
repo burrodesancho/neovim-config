9a0f37d839fc823386b57673200eb14d980e4bc9
Slaven Begović <slavenbeg@gmail.com>
2024-05-30 06:33:11 +0200

    Add some nice mappings: moving selected lines, replacing words...

--- lua/options.lua
+++ lua/options.lua
@@ -22,6 +22,13 @@ vim.opt.termguicolors = true -- enable true color support
 vim.opt.scrolloff = 8        -- minimum number of lines to keep above and below the cursor
 vim.opt.sidescrolloff = 8    --minimum number of columns to keep above and below the cursor
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+-- vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+vim.opt.undofile = true
+
+vim.opt.termguicolors = true
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
   callback = function()
@@ -47,6 +54,16 @@ vim.keymap.set({ "n", "v" }, "<leader>y", '[["+y]]')
 vim.keymap.set("n", "<leader>p", ":bp<CR>") -- previous buffer
 vim.keymap.set("n", "<leader>n", ":bn<CR>") -- next buffer
 vim.keymap.set("n", "<leader>x", ":bd<CR>") -- close the buffer
+
+-- Move selected lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep the cursor at the same place after joining lines
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Replace the word at current position
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
