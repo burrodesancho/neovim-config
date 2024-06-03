a428463bb2da9e47e7392cfec6a6d5dbb4e7a5fa
Slaven Begović <slavenbeg@gmail.com>
2024-06-03 07:10:28 +0200

    Fix colorscheme.lua and add integrations

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,11 +3,23 @@ return {
   lazy = false,
   name = "catppuccin",
   priority = 1000,
-  transparent_background = true,
-  styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
-    comments = { "italic" },     -- Change the style of comments
-  },
   config = function()
+    require("catppuccin").setup({
+      transparent_background = true,
+      styles = {                         -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" },         -- Change the style of comments
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        mason = true,
+        native_lsp = { enabled = true },
+        neotree = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+      },
+    })
     vim.cmd.colorscheme("catppuccin-mocha")
   end,
 }
