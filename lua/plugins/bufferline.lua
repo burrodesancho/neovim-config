e818482b8ba69b8b0dffd43c4736b9bbace4b1fd
Slaven Begović <slavenbeg@gmail.com>
2024-06-03 07:12:04 +0200

    Add catppuccin colorscheme to bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,23 +1,23 @@
 return {
-    "akinsho/bufferline.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    after = "catppuccin",
-    config = function()
-        require("bufferline").setup({
-            highlights = require("catppuccin.groups.integrations.bufferline").get(),
-            options = {
-                mode = "buffers",
-                offsets = {
-                    {
-                        filetype = "neo-tree",
-                        text = "NeoTree",
-                        highlight = "Directory",
-                        separator = true,
-                    },
-                },
-            },
-        })
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  after = "catppuccin",
+  config = function()
+    require("bufferline").setup({
+      highlights = require("catppuccin.groups.integrations.bufferline").get(),
+      options = {
+        mode = "buffers",
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "NeoTree",
+            highlight = "Directory",
+            separator = true,
+          },
+        },
+      },
+    })
 
-        vim.keymap.set("n", "<leader>bl", ":BufferLinePick<CR>")
-    end,
+    vim.keymap.set("n", "<leader>bl", ":BufferLinePick<CR>")
+  end,
 }
