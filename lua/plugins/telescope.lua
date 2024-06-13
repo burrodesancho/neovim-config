96fb73560a59c2b98c47b42d842aa66ceecd44ea
Slaven Begović <slavenbeg@gmail.com>
2024-06-13 18:05:44 +0200

    Add fzf to telescope.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-ui-select.nvim",
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
   },
   event = "VeryLazy",
   config = function()
@@ -20,10 +21,17 @@ return {
         ["ui-select"] = {
           require("telescope.themes").get_dropdown({}),
         },
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        }
       },
     })
 
     require("telescope").load_extension("ui-select")
+    require("telescope").load_extension("fzf")
 
     local builtin = require("telescope.builtin")
 
