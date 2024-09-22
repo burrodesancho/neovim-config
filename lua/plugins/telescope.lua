c97cdd47de099d65542087dba4a239d7a39dabda
Slaven Begovic <slavenbeg@gmail.com>
2024-09-22 08:48:14 +0200

    Add telescope buffer picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -38,6 +38,7 @@ return {
     local builtin = require("telescope.builtin")
 
     vim.keymap.set("n", "<leader>fs", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
     vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
     vim.keymap.set("n", "<leader>fz", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>fp", builtin.git_files, {})
