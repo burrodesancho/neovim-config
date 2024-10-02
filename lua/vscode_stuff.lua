625ac40bd3b966a97d9ae889fff24c03a64e63ad
burrodesancho <34947303+burrodesancho@users.noreply.github.com>
2024-10-02 09:14:21 +0200

    Add lsp keybindings for vscode

--- lua/vscode_stuff.lua
+++ lua/vscode_stuff.lua
@@ -7,4 +7,11 @@ return {
       require("nvim-surround").setup({})
     end,
   },
+  {
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" }),
+    vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Goto Definition" }),
+    vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Goto reference" }),
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" }),
+    vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, { desc = "Workspace Symbol" }),
+  },
 }
