c545e328ec014298426fdece0d0704e480b95a1b
Slaven Begovic <slavenbeg@gmail.com>
2024-10-23 04:34:20 +0200

    Add Tokynight colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,29 +1,71 @@
 return {
-  "catppuccin/nvim",
-  enabled = true,
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      compile_path = vim.fn.stdpath "cache" .. "/catppuccin",
+  {
+    "catppuccin/nvim",
+    enabled = false,
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
 
-      transparent_background = true,
-      styles = {                         -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" },         -- Change the style of comments
-      },
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        mason = true,
-        native_lsp = { enabled = true },
-        neotree = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        notify = true,
-      },
-    })
-    vim.cmd.colorscheme("catppuccin-mocha")
-  end,
+        transparent_background = true,
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" }, -- Change the style of comments
+        },
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          mason = true,
+          native_lsp = { enabled = true },
+          neotree = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+          notify = true,
+        },
+      })
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    enabled = true,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      local bg = "#011628"
+      local bg_dark = "#011423"
+      local bg_highlight = "#143652"
+      local bg_search = "#0A64AC"
+      local bg_visual = "#275378"
+      local fg = "#CBE0F0"
+      local fg_dark = "#B4D0E9"
+      local fg_gutter = "#627E97"
+      local border = "#547998"
+
+      require("tokyonight").setup({
+        style = "night",
+        on_colors = function(colors)
+          colors.bg = bg
+          colors.bg_dark = bg_dark
+          colors.bg_float = bg_dark
+          colors.bg_highlight = bg_highlight
+          colors.bg_popup = bg_dark
+          colors.bg_search = bg_search
+          colors.bg_sidebar = bg_dark
+          colors.bg_statusline = bg_dark
+          colors.bg_visual = bg_visual
+          colors.border = border
+          colors.fg = fg
+          colors.fg_dark = fg_dark
+          colors.fg_float = fg
+          colors.fg_gutter = fg_gutter
+          colors.fg_sidebar = fg_dark
+        end,
+      })
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
 }
