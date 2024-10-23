e75464e42c58168621245dec87cb2d98d2aaa316
Slaven Begovic <slavenbeg@gmail.com>
2024-10-23 04:38:23 +0200

    Disable none-ls. Use conform and nvim-lint

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,31 @@
 return {
   "nvimtools/none-ls.nvim",
-  enabled = true,
+  enabled = false,
   config = function()
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     local null_ls = require("null-ls")
+
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.completion.luasnip,
         null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.formatting.ruff,
-        null_ls.builtins.formatting.black,
+        -- null_ls.builtins.formatting.ruff,
+        -- null_ls.builtins.formatting.black,
       },
+      -- you can reuse a shared lspconfig on_attach callback here
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
     })
 
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format File" })
