12d1d20e8084c0c78b74ee3d82a1ff8bfc118152
Slaven Begovic <slavenbeg@gmail.com>
2024-10-23 04:39:53 +0200

    Move mason stuff to a separate file

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,32 @@
+return {
+  "williamboman/mason.nvim",
+  enabled = true,
+  cmd = "Mason",
+  event = "BufReadPre",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
+
+    mason.setup({
+      ui = { border = "rounded" },
+    })
+
+    mason_lspconfig.setup({
+
+      ensure_installed = { "lua_ls", "pyright", "ruff" },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "stylua",
+        "isort",
+        "mypy",
+      },
+    })
+  end,
+}
