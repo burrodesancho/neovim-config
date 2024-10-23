cc20afa92c935c85d270bec65af3039b61dfeb06
Slaven Begovic <slavenbeg@gmail.com>
2024-10-23 04:36:10 +0200

    Formatting

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -27,6 +27,9 @@ return {
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
+        completion = {
+          comleteopt = "menu,menuone,preview,noselect",
+        },
         snippet = {
           expand = function(args)
             require("luasnip").lsp_expand(args.body)
@@ -60,7 +63,7 @@ return {
             name = "nvim_lsp",
             entry_filter = function(entry)
               return require("cmp").lsp.CompletionItemKind.Snippet ~= entry:get_kind()
-            end
+            end,
           },
           { name = "luasnip" },
         }, {
@@ -69,13 +72,13 @@ return {
         }),
         formatting = {
           format = lspkind.cmp_format({
-            mode = "symbol_text",             -- show only symbol annotations
+            mode = "symbol_text", -- show only symbol annotations
             -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
             -- can also be a function to dynamically calculate max width such as
             -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
             maxwidth = 50,
-            ellipsis_char = "...",                -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-            show_labelDetails = true,             -- show labelDetails in menu. Disabled by default
+            ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
             menu = {
               buffer = "[Buffer]",
               nvim_lsp = "[LSP]",
@@ -88,18 +91,18 @@ return {
       })
 
       -- `:` cmdline setup.
-      cmp.setup.cmdline(':', {
+      cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
-          { name = 'path' }
+          { name = "path" },
         }, {
           {
-            name = 'cmdline',
+            name = "cmdline",
             option = {
-              ignore_cmds = { 'Man', '!' }
-            }
-          }
-        })
+              ignore_cmds = { "Man", "!" },
+            },
+          },
+        }),
       })
     end,
   },
